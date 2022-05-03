bf88a0597c99a9c9792c92d49d5043f6b75796fc
alexmarin86 <alexeu86@gmail.com>
2022-05-03 12:15:43 +0200

    third commit

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const _ = require('lodash')
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log('request event')
+    res.end('Hello world')
+})
+server.listen(5000, () => {
+    console.log('server listening on port 5000...')
+})
